5898b77a661c5a98ead2325c88c3b2a857414816
BrandiBQ <35387365+BrandiBQ@users.noreply.github.com>
2018-04-06 14:56:35 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,88 @@
 ﻿/**
  * Created by bcuser on 4/4/18.
- */ 
+ */
 var dice1;
 var dice2;
-var winOrLose;
-var rollValue = dice1 + dice2;
-var userBalance = 5;
-var balance = document.getElementById("balance"); 
-var currentTurn = 0;
-var turnCount = document.getElementById("turnCount");
-var status = document.getElementById("status");
-var message = "GAME OVER!";
-var ButtonBet = document.getElementById("ButtonBet");
-status.style.display = 'none';
 
 function buttonClicked() {
-    if (userBalance > 0) { // checking that the userBalance is greater than 0 (#5 of homework)
-
-        // ====================================================================================
-        // #1 of homework instructions - calculate 2 random integers for the dice.
-        // ====================================================================================
-        dice1 = Math.floor(Math.random() * 6) + 1;
-        dice2 = Math.floor(Math.random() * 6) + 1;
-        console.log("This is dice 1: " + dice1);
-        console.log("This is dice 2: " + dice2);
-        // moved the end of the buttonClicked clicked function to here
-
-
-
-        // ====================================================================================
-        // #2 of homework instructions - change dice images based on their value when rolled.
-        // ====================================================================================
-        //      I think this switch statement should be in its own function, but for now, I 
-        //      just wanted to get this working and then when the assignment is finished, we can step
-        //      back to look at everything and see how best to organize.
-        switch(dice1) {
-            case 1:
-                document.getElementById("image1").src = "./images/dice-1.jpg";
-                break;
-            case 2:
-                document.getElementById("image1").src = "./images/dice-2.jpg";
-                break;
-            case 3:
-                document.getElementById("image1").src = "./images/dice-3.jpg";
-                break;
-            case 4:
-                document.getElementById("image1").src = "./images/dice-4.jpg";
-                break;
-            case 5:
-                document.getElementById("image1").src = "./images/dice-5.jpg";
-                break;
-            case 6:
-                document.getElementById("image1").src = "./images/dice-6.jpg";
-                break;
-            default:
-                console.log("something's wrong with dice1.");
-        } // end of dice 1 switch statement
+    // ====================================================================================
+    // #1 of homework - calculate 2 random integers for the dice.
+    // ====================================================================================
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2 = Math.floor(Math.random() * 6) + 1;
+    console.log("This is dice 1: " + dice1);
+    console.log("This is dice 2: " + dice2);
     
-        switch(dice2) {
-            case 1:
-                document.getElementById("image2").src = "./images/dice-1.jpg";
-                break;
-            case 2:
-                document.getElementById("image2").src = "./images/dice-2.jpg";
-                break;
-            case 3:
-                document.getElementById("image2").src = "./images/dice-3.jpg";
-                break;
-            case 4:
-                document.getElementById("image2").src = "./images/dice-4.jpg";
-                break;
-            case 5:
-                document.getElementById("image2").src = "./images/dice-5.jpg";
-                break;
-            case 6:
-                document.getElementById("image2").src = "./images/dice-6.jpg";
-                break;
-            default:
-                console.log("something's wrong with dice2.");
-        } // end of dice 2 switch statement
+    // ====================================================================================
+    // #2 of homework - change dice images based on their value when rolled.
+    // ====================================================================================
+    diceImage("image1", dice1);
+    diceImage("image2", dice2);
+    
+    // ====================================================================================
+    // #3 of homework - determine win/lose for each roll and display win/lose text.
+    // ====================================================================================
+    //      Must leave these variables here, don't know why. If these variables are moved to either
+    //      the top of the function or the top of the script, the code breaks. This would be
+    //      a good code block to put in a function too. I did not do that though in case you
+    //      would like to try, for practice. If not, no worries, it's not required in the assignment.
+    var winOrLose;
+    var rollValue = dice1 + dice2;
+    var balance = document.getElementById("balance").innerHTML;
+    var turnCount = document.getElementById("turnCount").innerHTML;    
+    var buttonBet = document.getElementById("ButtonBet");    
+    
+    
+    // user wins if double, 7, or 11 is rolled 
+    if(dice1 === dice2 || rollValue === 7 || rollValue === 11) {
+        winOrLose = true;
+        document.getElementById("status").innerHTML = "You win!";
+        balance++;
+        console.log(winOrLose);
+    } else {
+        winOrLose = false;
+        document.getElementById("status").innerHTML = "You lose!";
+        balance--;
+        console.log(winOrLose);
+    } // end of if/else statement
+    
+    // ====================================================================================
+    // #4 of homework - update $ balance and number of turns
+    // ====================================================================================
+    document.getElementById("balance").innerHTML = balance;
+    document.getElementById("turnCount").innerHTML = ++turnCount;
+    
+    // ====================================================================================
+    // #5 of homework - when user balance reached 0, hide button and update game status
+    // ====================================================================================    
+    if(balance === 0){
+        buttonBet.style.display = "none";
+        document.getElementById("status").innerHTML = "GAME OVER!";
+    }
+}; // end of buttonClicked function
 
-    // ====================================================================================
-    // #3 of homework instructions - determine a win or lose for each roll.
-    // #4 of homework instructions - adjusting balance and turn counter
-    // ====================================================================================
-        rollValue = dice1 + dice2; // calculating the total value of the dice rolled
-        if(dice1 === dice2 || rollValue === 7 || rollValue === 11) {
-            winOrLose = true;
-            console.log(`player winOrLose in if ${winOrLose}`);
-            userBalance = userBalance + 1;
-            console.log(`balance in if statement $${userBalance}`);
-            currentTurn = currentTurn + 1;
-        }
-        else {
-            winOrLose = false;
-            console.log(`player winOrLose in else ${winOrLose}`);
-            userBalance = userBalance - 1;
-            console.log(`player balance is $${userBalance}`);
-            currentTurn = currentTurn +1;
-        } // end of nested if statement
-        balance.innerHTML = userBalance;
-        turnCount.innerHTML = currentTurn;
-        if (userBalance === 0) {
-            ButtonBet.style.display = 'none'; // hides the button however it hides if after one more click
-            status.style.display = 'block'; // the message is not displaying
-        }
-    } // end of main if statement
-} // end of buttonClicked function
+// function for updating dice image based on value rolled
+function diceImage(HTMLimageID, dice){    
+    switch(dice) {
+        case 1:
+            document.getElementById(HTMLimageID).src = "./images/dice-1.jpg";
+            break;
+        case 2:
+            document.getElementById(HTMLimageID).src = "./images/dice-2.jpg";
+            break;
+        case 3:
+            document.getElementById(HTMLimageID).src = "./images/dice-3.jpg";
+            break;
+        case 4:
+            document.getElementById(HTMLimageID).src = "./images/dice-4.jpg";
+            break;
+        case 5:
+            document.getElementById(HTMLimageID).src = "./images/dice-5.jpg";
+            break;
+        case 6:
+            document.getElementById(HTMLimageID).src = "./images/dice-6.jpg";
+            break;
+        default:
+            console.log("something's wrong with the dice image.");    
+    }
+}
